46e83bf072a7dffbdfc213d4c789ae93cbe879c2
shs3182ym <yeongmins030@gmail.com>
2020-01-15 19:30:10 +0900

    Base Code ADDED
    commnad: test

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const testBot = new Discord.Client()
+const message = testBot.message
+const fs = require("fs")
+const version = fs.readFileSync("version.txt")
+const config = fs.readFileSync("config.txt").split("\n")
+
+testBot.on('ready', () => {
+  console.log('Bot Version '+version+" ONLINE!")
+})
+
+testBot.on("message", (message) => {
+    var commands = message.content.split(" ")
+    try{
+        switch(commands[0]){
+
+            case config[0]+"test":
+                message.reply("Success!")
+
+        }
+    }catch(e){
+        console.log("An Error occurred:\n"+e)
+    }
+})

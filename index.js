1ef7311e2a0b307241d4698ae4e46d89991b237f
shs3182ym <yeongmins030@gmail.com>
2020-01-15 20:55:10 +0900

    command bug PATCHED

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const testBot = new Discord.Client()
 const message = testBot.message
 const fs = require("fs")
 const version = fs.readFileSync("version.txt")
-const config = fs.readFileSync("config.txt").split("\n")
+const config_raw = fs.readFileSync("config.txt")
+const config = config_raw.toString().split("\n")
 
 testBot.on('ready', () => {
-  console.log('Bot Version '+version+" ONLINE!")
+  console.log('Bot Version '+version+" ONLINE!\nPREFIX: "+config[0].split("=")[1])
 })
 
 testBot.on("message", (message) => {
     var commands = message.content.split(" ")
+    var cmd_main=config[0].split("=")[1]+commands[0]
     try{
-        switch(commands[0]){
-
-            case config[0].split("=")[1]+"test":
-                message.reply("Success!")
-
-        }
+        console.log(message.author+": "+message.content)
+        if(message.content=="WMtest")
+            message.reply("Success!")
     }catch(e){
         console.log("An Error occurred:\n"+e)
     }

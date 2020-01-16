e85a714d4e6f34319a0bc5ebd9f4d70c245e8018
shs3182ym <yeongmins030@gmail.com>
2020-01-16 19:10:14 +0900

    vote ADDED-WORK_IN_PROGRESS
    non-stable!

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config_raw = fs.readFileSync("config.txt")
 const config = config_raw.toString().split("\n")
 var chosung_quest
 var chosung_answer
+var vote_table
 
 testBot.on('ready', () => {
   console.log('Bot Version '+version+" ONLINE!\nPREFIX: "+config[0].split("=")[1])
@@ -16,7 +17,8 @@ testBot.on("message", (message) => {
     var commands = message.content.replace(config[0].split("=")[1],"").split(" ")
     var cmd_main=commands[0]
     try{
-        console.log(message.author+": "+message.content)
+
+        console.log(message.author.username+": "+message.content)
         if(cmd_main=="WMtest"){
             message.reply("Success!")
         }
@@ -50,6 +52,30 @@ testBot.on("message", (message) => {
             }else{
                 message.reply(config[0].split("=")[1]+"초성퀴즈 출제 명령어를 입력해서 문제를 출제하거나 "+config[0].split("=")[1]+"초성퀴즈 정답 명령어로 초성문제의 정답을 입력하세요!")
             }
+        }
+
+        if(cmd_main=="WM투표"){
+            if(commnads[1]=="생성"){
+                if(commands[2].length>20){
+                    message.reply("투표 제목은 20글자까지만 가능합니다.")
+                }else{
+                    title = commands[2]
+                    commands.shift()
+                    commands.shift()
+                    commands.shift()
+                    var voteid_tmp = Math.round(Math.random()*99999)
+                    var count_of_zero = 5-(voteid_tmp.toString().length)
+                    var voteid = "0".repeat(count_of_zero)+voteid_tmp.toString()
+                    vote_table.push([voteid, title, commands[3], 0, commands[4], 0])
+                    message.reply(title+"(이)라는 제목을 가진 투표가 생성되었습니다.")
+                }
+            }else if(commands[1]=="투표"){
+                message.reply("W.I.P. Work In Progress")
+            }
+        }
+
+        if(cmd_main=="WM도움말"){
+            message.reply("Help: \nWMtest: 봇 작동 테스트 ||| WM초성퀴즈: 초성퀴즈 ||| WM투표: 이지선다 투표(현재 개발 중)")
         }
         
     }catch(e){
